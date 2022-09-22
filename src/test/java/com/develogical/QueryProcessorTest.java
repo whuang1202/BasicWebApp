3110f4413a03e3488f24ba2f76c4648036e48d0f
William Huang <whuan@ASUS-GL553VD.localdomain>
2022-09-21 22:18:39 -0400

    y

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -21,6 +21,11 @@ public class QueryProcessorTest {
     }
 
     @Test
+    public void knowsAboutASD() throws Exception {
+        assertThat(queryProcessor.process("asd"), containsString("blegh"));
+    }
+
+    @Test
     public void isNotCaseSensitive() throws Exception {
         assertThat(queryProcessor.process("shakespeare"), containsString("playwright"));
     }
